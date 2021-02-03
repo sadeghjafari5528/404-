03dba26b9f627e57db7a209e3bc426de22752b9c
sadeghjafari5528 <sadeghjafari5528@gmail.com>
2021-02-03 11:38:41 +0330

    x

--- Back/generalchatroom/urls.py
+++ Back/generalchatroom/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('generalchatroom/', views.index, name='index'),
     path('generalchatroom/<str:room_name>/', views.room, name='room'),
     path('show_Message/', views.show_Message, name='show_Message'),
 ]
